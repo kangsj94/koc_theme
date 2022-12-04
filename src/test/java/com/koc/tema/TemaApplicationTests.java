e273f645072d720cada5d0de00c1de36ca0083c8
seongjo24 <kangsj24@naver.com>
2022-12-04 22:01:27 +0900

    first commit

---
+++ src/test/java/com/koc/tema/TemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koc.tema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
